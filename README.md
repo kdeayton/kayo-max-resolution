d6087cdb9776ae7bdf88c26a657337738a726c5a
Keith Deayton <keith.deayton@gmail.com>
2024-07-15 11:45:34 +1000

    Made compatible with Firefox, tested in Firefox/Chrome OK, Opera NOT OK...

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A Chrome Extension that forces the maximum resolution for Kayo Sports to be applied
 
-Highly inspired by <a href="https://github.com/logore/amazon-prime-video-1080p/">Amazon Prime Video 1080p</a>
+Highly inspired by <a href="https://github.com/logore/amazon-prime-video-1080p/">Amazon Prime Video 1080p</a>. Many thanks!
 <br>
 
 ## How does it work?
@@ -17,9 +17,12 @@ Manually... Check the video on the inspired Repository
 
 ## Support
 
-#### Chromium browsers
-- Google Chrome
-- Opera
+#### Chromium browsers 
+- Google Chrome; works well!
+- Opera (doesn't seem to properly work, haven't found a solution, may not find a solution...)
+
+#### Other browsers
+- Firefox; works well!
 
 #### OS
 - Windows
@@ -31,7 +34,7 @@ Manually... Check the video on the inspired Repository
 <br>
 
 ## Information
-Not tested in all Chromium browsers
+Not tested in all Chromium browsers; only those mentioned above.
 
 Also, I accept issues, so please feel free to open an issue!
 
--- js/content_script.js
+++ js/content_script.js
@@ -33,49 +33,48 @@ const main = () => {
         script.addEventListener('load', () => {
           xhook.after(function(request, response) {
             try {
-              if (request.url.match(/\.mpd/)) {
+              if (request.url.match(/\index.mpd/)) {
                 let mpd = response.text
+				//console.log("MPD original: "+mpd.length)
 
                 const parser = new DOMParser()
                 const dom = parser.parseFromString(mpd, 'text/xml')
     
                 let periods = dom.querySelectorAll('Period')
 
-                let periodIndex = periods.length == 1 ? 0 : 1
-                let period = periods[periodIndex]
-                let representations = period.querySelectorAll('AdaptationSet[mimeType="video/mp4"] > Representation')
+				periods.forEach((period, periodIndex) => {	
+                  let representations = period.querySelectorAll('AdaptationSet[mimeType="video/mp4"] > Representation')
 
-                let heights = []
-                let bandwidths = []
-                let ids = []
+                  let heights = []
+                  let bandwidths = []
+                  let ids = []
               
-                representations.forEach(rep => {
-                  heights.push(parseInt(rep.getAttribute('height'), 10))
-                  bandwidths.push(parseInt(rep.getAttribute('bandwidth'), 10))
+                  representations.forEach(rep => {
+                    heights.push(parseInt(rep.getAttribute('height'), 10))
+                    bandwidths.push(parseInt(rep.getAttribute('bandwidth'), 10))
 
-                  ids.push(rep.getAttribute('id'))
-                })
+                    ids.push(rep.getAttribute('id'))
+                  })
 
-                // sort by largest number
-                heights = Array.from(new Set(heights))
-                heights.sort((a, b) => b - a)
+                  // sort by largest number
+                  heights = Array.from(new Set(heights))
+                  heights.sort((a, b) => b - a)
 
-                // sort by largest number
-                bandwidths = Array.from(new Set(bandwidths))
-                bandwidths.sort((a, b) => b - a)
+                  // sort by largest number
+                  bandwidths = Array.from(new Set(bandwidths))
+                  bandwidths.sort((a, b) => b - a)
 
-                ids.forEach((id, i) => {
-                  if (!(representations[i].getAttribute('height') == heights[0].toString() && representations[i].getAttribute('bandwidth') == bandwidths[0].toString())) {
-                    // there may be multiple Representations with a height of 1080, so maybe there should be an option to allow the user to choose those.
-                    // the maximum bandwidth will cause the file size to be large, so it will be overloaded.
-
-                    // delete other representation elements
-                    dom.querySelectorAll('Period')[periodIndex].querySelector(`AdaptationSet[mimeType="video/mp4"] > Representation[id="${id}"]`).remove()
-                  }
-                })
+                  ids.forEach((id, i) => {
+                    if (!(representations[i].getAttribute('height') == heights[0].toString() && representations[i].getAttribute('bandwidth') == bandwidths[0].toString())) {
+                      // delete other representation elements
+				  	  dom.querySelectorAll('Period')[periodIndex].querySelector(`AdaptationSet[mimeType="video/mp4"] > Representation[id="${id}"]`).remove()
+                    }
+                  })
+				})
 
                 // revert the edited mpd xml to plain mpd
                 let mpd_ = dom.documentElement.outerHTML
+				//console.log("MPD modified: "+mpd_.length)
       
                 response.text = mpd_
               }
